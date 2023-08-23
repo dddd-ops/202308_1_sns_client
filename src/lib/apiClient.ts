3364fed50580c753066749effcb51985105ea839
unknown <for.contact.k@gmail.com>
2023-08-24 02:18:59 +0900

    profile-image

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    //baseURL: "http://localhost:8000/api",
-    baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+    // baseURL: "http://localhost:10000/api",
+   baseURL: "http://localhost:10000/api" , 
     headers:{
         "Content-Type": "application/json",
     },
 });
 
 export default apiClient;
+
+
+//baseURL: "http://localhost:8000/api" || process.env.NEXT_PUBLIC_API_BASEURL,
