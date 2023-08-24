0d34c71c59719c8190c4f6e9d9310e05c2f3a5a9
unknown <for.contact.k@gmail.com>
2023-08-25 05:35:28 +0900

    add apiClientPubURL

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     // baseURL: "http://localhost:10000/api",
-   baseURL: "http://localhost:10000/api" , 
+   baseURL: "http://localhost:10000/api" || process.env.NEXT_PUBLIC_API_BASEURL, 
     headers:{
         "Content-Type": "application/json",
     },
